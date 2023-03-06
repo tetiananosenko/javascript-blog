062a37d47c628cf3122e1be20dff38c078b96455
Tetiana Nosenko <tanya2016english@gmail.com>
2023-03-06 12:09:56 +0100

    create code js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 'use strict'
 
 function titleClickHandler(event) {
-    console.log(event);
+    event.preventDefault();
+    const clickedElement = this;
 
-    /* remove class 'active' from all article links  */
+   
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-    /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
 
-    /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
-    /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
